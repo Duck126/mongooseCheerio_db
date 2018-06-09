e2d68f76a7bd1f4b23661273c135d2982e2c9a90
Duck126 <Duck126@hotmail.com>
2018-06-09 11:09:21 -0500

    heroku fix

--- public/js/news.js
+++ public/js/news.js
@@ -2,31 +2,21 @@ $.getJSON("/news", function (news) {
     for (var i = 0; i < news.length; i++) {
         var dropNum = 0;
         dropNum++;
-        $("#articlesDiv").append("<li class='collection-item avatar'> <img src='https://picsum.photos/200/300/?random' alt='" +
-            news[i].title + "' class='circle'> <a href='" +
-            news[i].link + "'><h4 class='title'> " +
-            news[i].title + "</h4></a> <p>" +
-            news[i].story + "</p> <a id='commentadd' data-id='" +
-            news[i]._id + "' href='#modal1' class='waves-effect waves-light btn modal-trigger'>" +
-            "<i class='material-icons'>comment</i></a>" +
-            "<a class='dropdown-trigger btn' id='showCom' data-id='" +
-            news[i]._id + "' data-target='dropdown1'>View Comments</a>" +
-            "<ul id='dropdown1' class='dropdown-content'></ul></li>");
-    }
+        if (news[i].link !== null ) {
+            $("#articlesDiv").append("<li class='collection-item avatar'> <img src='https://picsum.photos/200/300/?random' alt='" +
+                news[i].title + "' class='circle'> <a href='" +
+                news[i].link + "'><h4 class='title'> " +
+                news[i].title + "</h4></a> <p>" +
+                news[i].story + "</p> <a id='commentadd' data-id='" +
+                news[i]._id + "' href='#modal1' class='waves-effect waves-light btn modal-trigger'>" +
+                "<i class='material-icons'>comment</i></a>" +
+                "<a class='dropdown-trigger btn' id='showCom' data-id='" +
+                news[i]._id + "' data-target='dropdown1'>View Comments</a>" +
+                "<ul id='dropdown1' class='dropdown-content'></ul></li>");
+        };
+    };
 });
 
-// <!-- Dropdown Trigger -->
-//   <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Drop Me!</a>
-
-//   <!-- Dropdown Structure -->
-//   <ul id='dropdown1' class='dropdown-content'>
-//     <li><a href="#!">one</a></li>
-//     <li><a href="#!">two</a></li>
-//     <li class="divider" tabindex="-1"></li>
-//     <li><a href="#!">three</a></li>
-//     <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-//     <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
-//   </ul>
 
 $(document).ready(function () {
     $('.modal').modal();
@@ -36,7 +26,7 @@ $(document).ready(function () {
 $(document).on("click", "#showCom", function () {
 
     var Id = $(this).attr("data-id");
-        console.log(Id);
+    console.log(Id);
     $.ajax({
         method: "GET",
         url: "/article/" + Id
@@ -53,7 +43,7 @@ $(document).on("click", "#commentadd", function () {
     $("#commentText").empty();
 
     var Id = $(this).attr("data-id");
-        
+
     $("#modal1").on("click", "#submitbtn", function (Id) {
         $.ajax({
                 method: "POST",
